c2f194a4ec0c7799f9223225a26d4d9cb600aea4
shivam907 <shivamkaushal907@gmail.com>
2020-10-16 01:41:50 +0530

    Updated

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -42,7 +42,7 @@ class Post(db.Model):
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    img_file = db.Column(db.String(20), nullable=False, default='default.jpg'), '{self.img_file}'
+    img_file = db.Column(db.String(20), nullable=False, default='default.jpg')
 
     def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}', '{self.img_file}')"
